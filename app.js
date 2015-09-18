d8fde97fd379eec83d1bd9d33a6f3034ab6f16b6
Lizzie Piazza <epiazza22@gmail.com>
2015-09-17 21:34:40 -0400

    Search is functional, content parsed

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 (function(){
-	var app = angular.module('wikiSearch', []);
+	var app = angular.module('wikiSearch', ['ngSanitize']);
 
-	app.controller('searchCtrl', ['$http', function($http){
-		var query = this;
-		query.input = "Hello";
-		query.submit = function(input){
-			alert(input);
+	app.controller('searchCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce){
+		$scope.input = "Hello";
+		$scope.submit = function(input){
+			$http.jsonp('https://en.wikipedia.org/w/api.php/w/api.php?action=query&list=search&format=json&callback=JSON_CALLBACK&srsearch=' + input + '&continue=')
+			.then(function(data){
+				$scope.response = data.data.query.search.map(function(element){
+					return {
+						'title': element.title,
+						'snippit': $sce.trustAsHtml(element.snippet),
+						'url': 'https://en.wikipedia.org/wiki/' + element.title.replace(/\s/g, '_')
+					};
+				});
+			});
 		};
 	}]);
 
