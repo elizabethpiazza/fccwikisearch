039faba1e36aae6059f567b387e1581e9bf7212f
Lizzie Piazza <epiazza22@gmail.com>
2015-09-19 11:54:17 -0400

    Added random button

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 	var app = angular.module('wikiSearch', ['ngSanitize']);
 
 	app.controller('searchCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce){
-		$scope.input = "Hello";
+		//$scope.input = "Hello";
 		$scope.submit = function(input){
+			if (input === undefined) {
+				alert('Please enter a search term.');
+				return;
+			}
 			$http.jsonp('https://en.wikipedia.org/w/api.php/w/api.php?action=query&list=search&format=json&callback=JSON_CALLBACK&srsearch=' + input + '&continue=')
 			.then(function(data){
 				$scope.response = data.data.query.search.map(function(element){
